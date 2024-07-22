10fd91c0898f7cd4a557aeba43dc978b4b27e47d
Hiro Mataba <aganzemataba@gmail.com>
2024-07-22 10:33:09 +0200

    Update Readme file

--- src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobilecomponent.ts
+++ src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobilecomponent.ts
@@ -3,6 +3,7 @@ import { MenuService } from '../../../services/menu.service';
 import { NavbarMobileMenuComponent } from './navbar-mobile-menu/navbar-mobile-menu.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { NgClass } from '@angular/common';
+import { PodcastListComponent } from "../../../../dashboard/components/podcast/podcast-list/podcast-list.component";
 
 @Component({
     selector: 'app-navbar-mobile',
@@ -10,10 +11,11 @@ import { NgClass } from '@angular/common';
     styleUrls: ['./navbar-mobile.component.scss'],
     standalone: true,
     imports: [
-        NgClass,
-        AngularSvgIconModule,
-        NavbarMobileMenuComponent,
-    ],
+    NgClass,
+    AngularSvgIconModule,
+    NavbarMobileMenuComponent,
+    PodcastListComponent
+],
 })
 export class NavbarMobileComponent implements OnInit {
   constructor(public menuService: MenuService) {}
